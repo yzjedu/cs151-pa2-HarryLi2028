6d9e71bd2999c21f00c0431a1a4a04bf4de6cdf3
HarryLi2028 <Hli2@loyola.edu>
2024-10-23 18:00:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,47 +1,57 @@
 # Output an intro of the program 
-print('Welecome to the game of sticks, In this game there will be 3 players including you, your friend and the computer.')
+print('Welcome to the game of sticks! In this game, there will be 3 players: you, your friend, and the computer.')
 print('The goal of this game is to not be the one to take the last stick!')
 
 # Import random module
 import random 
 
-# Create variables 
-player_1 = 0
-player_2 = 0
-player_3 = 0
-repeat = str('yes')
+# Create variables to track wins
+player_1_loss = 0
+player_2_loss = 0
+player_3_loss = 0
+repeat = 'yes'
 
 # Create a while loop to repeat when the user wants to play again
 while repeat == 'yes':
-    total_sticks = random.randint(10,100)
+    # Randomly select a number of sticks between 10 and 100
+    total_sticks = random.randint(10, 100)
+    print(f'The game starts with {total_sticks} sticks.')
+    
     while total_sticks > 0:
-        if total_sticks > 0:
-            choice_1 = int(input('Player 1 choose a number between 1 and 3:'))
-            while choice_1 <= 0 and choice_1 >= 4:
-                choice_1 = int(input('Player 1, choose a valid number between 1 and 3:'))
-            total_sticks -= choice_1
-            if total_sticks <= 0:
-                player_1 += 1
-                print('player one has taken the last stick.')
-        if total_sticks > 0:
-            choice_2 = int(input('Player 2 choose a number between 1 and 3:'))
-            while choice_2 <= 0 and choice_2 >= 4:
-                choice_2 = int(input('Player 2, choose a valid number between 1 and 3:'))
-            total_sticks -= choice_2
-            if total_sticks <= 0: 
-                player_2 += 1
-                print('player 2 has taken the last stick')
-        if total_sticks > 0:
-            choice_3 = random.randint(1,3)
-            total_sticks -= choice_3
-            if total_sticks <= 0:
-                player_3 += 1
-                print('Player 3 has taken the last stick')
-    if total_sticks < 0:
-        repeat = str(input('Do you want to play again:'))
-        while repeat != 'no' and repeat != 'yes':
-            repeat = str(input('Please enter yes or no:'))
-            repeat = repeat.lower()
-        if repeat == 'no':
-            print('Thanks for playing!')
-            
+        # Player 1's turn
+        choice_1 = int(input('Player 1, choose a number between 1 and 3: '))
+        while choice_1 < 1 or choice_1 > 3:
+            choice_1 = int(input('Player 1, choose a valid number between 1 and 3: '))
+        total_sticks -= choice_1
+        if total_sticks <= 0:
+            player_1_loss += 1
+            print('Player 1 has taken the last stick and lost the game!')
+            break
+
+        # Player 2's turn
+        choice_2 = int(input('Player 2, choose a number between 1 and 3: '))
+        while choice_2 < 1 or choice_2 > 3:
+            choice_2 = int(input('Player 2, choose a valid number between 1 and 3: '))
+        total_sticks -= choice_2
+        if total_sticks <= 0:
+            player_2_loss += 1
+            print('Player 2 has taken the last stick and lost the game!')
+            break
+
+        # Player 3 (Computer)'s turn
+        choice_3 = random.randint(1, 3)
+        print(f'Player 3 (Computer) chooses: {choice_3}')
+        total_sticks -= choice_3
+        if total_sticks <= 0:
+            player_3_loss += 1
+            print('Player 3 (Computer) has taken the last stick and lost the game!')
+            break
+
+    # Ask if the players want to play again
+    print('player 1 lost:',player_1_loss)
+    print('player 2 lost:',player_2_loss)
+    print('player 3 lost:',player_3_loss)
+    repeat = input('Do you want to play again (yes or no)? ').lower()
+    while repeat != 'no' and repeat != 'yes':
+        repeat = input('Please enter yes or no: ').lower()
+
